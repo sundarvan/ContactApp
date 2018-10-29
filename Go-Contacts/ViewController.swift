4bb619d3741a9e9e1364d7ed8657edcc6c16fbdb
jowin <jowinkoh94@gmail.com>
2018-10-29 15:58:44 +0800

    sundar_commit_create_new_project_for_contact_app

---
+++ Go-Contacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Go-Contacts
+//
+//  Created by Sundar on 10/29/18.
+//  Copyright © 2018 sundar. All rights reserved.
+// changes
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
